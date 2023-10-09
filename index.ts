8402d5b8153ad89afdc61acad573d0647bbaded2
Kieran Smith <kieran.jas.smith@gmail.com>
2023-10-09 19:15:30 +1000

    Special method impl

--- index.ts
+++ index.ts
@@ -1,43 +1,71 @@
 
-const P = "p"
-    
 type HTMLElementTypeMap = {
     p: HTMLParagraphElement,
     h1: HTMLHeadingElement,
+    h2: HTMLHeadingElement,
+    h3: HTMLHeadingElement,
+    h4: HTMLHeadingElement,
+    h5: HTMLHeadingElement,
+    h6: HTMLHeadingElement,
+    span: HTMLSpanElement,
+    ul: HTMLUListElement,
 }
-
-class VirtElement<const T extends keyof HTMLElementTypeMap> {
-    type!: keyof HTMLElementTypeMap
-    element!: HTMLElementTypeMap[T extends keyof HTMLElementTypeMap ? T : never]
-    children!: ChildrenRecord
-    events!: EventRecord<DomEventType>
-    
-    constructor(type: T) {
-        this.element = document.createElement(type) as HTMLElementTypeMap[T extends keyof HTMLElementTypeMap ? T : never]
-    }
-
-    // static of<T extends string & HTMLElementType>(): VirtElement<T> {
-    //     return new VirtElement<>(T)
-    // }
-}
-
-type VirtElementRecord<T extends keyof HTMLElementTypeMap> = Record<T, VirtElement<T>>
-type ChildrenRecord = Record<keyof HTMLElementTypeMap, VirtElementRecord<keyof HTMLElementTypeMap>>
-type EventRecord<T extends DomEventType> = Record<T, (event: DomEventTypeMap[T]) => void>
-type DomEventType = "onClick" | "mouseEnter"
 
 type DomEventTypeMap = {
     onClick: PointerEvent,
     mouseEnter: MouseEvent
 }
 type DomEvent =
-    | {type: "onClick", event: PointerEvent}
-    | {type: "mouseEnter", event: MouseEvent}
+    | { type: "onClick", event: PointerEvent }
+    | { type: "mouseEnter", event: MouseEvent }
+
+type VElementChildren = {
+    [key in keyof HTMLElementTypeMap]?: VElement.VElement<key>
+}
+
+type VElementEventCallbacks = {
+    [key in keyof DomEventTypeMap]?: (event: DomEventTypeMap[key]) => void
+}
+
+type VElementSpecialMethods = {
+    ul: { add: typeof VElement.create }
+}
+
+namespace VElement {
+    export declare interface VElement<T extends string & keyof HTMLElementTypeMap> {
+        type: keyof HTMLElementTypeMap,
+        element: HTMLElementTypeMap[T],
+        children: VElementChildren,
+        events: VElementEventCallbacks,
+        methods: VElementSpecialMethods[T extends keyof VElementSpecialMethods ? T : never]
+    }
+
+    function implSpecial<const T extends keyof HTMLElementTypeMap>(type: T): VElementSpecialMethods[T extends keyof VElementSpecialMethods ? T : never] {
+        switch (type) {
+            case "ul":
+                return { add: create } as VElementSpecialMethods["ul"]
+            default:
+                return {} as VElementSpecialMethods[never]
+        }
+    }
+
+    export function create<const T extends keyof HTMLElementTypeMap>(type: T, content: string = ""): VElement<T> {
+        let vElement: VElement<T> = {
+            type: type,
+            element: document.createElement(type) as HTMLElementTypeMap[T extends keyof HTMLElementTypeMap ? T : never],
+            children: {},
+            events: {},
+            methods: implSpecial(type)
+        }
+        vElement.element.textContent = content;
+        return vElement;
+    }
+}
 
 
 interface ElementSpawner {
 
 }
 
-let x = new VirtElement("h1")
-x.children.h1
+let x = VElement.create("ul")
+let b = x.methods.add("h1")
